3828d4f834377c7a3111aec9d5f461a8e49ab1df
SUPerchokki <32721115+SUPerchokki@users.noreply.github.com>
2022-09-21 03:33:31 -0300

    termino do codigo de newton rapshon

--- newton-raphson.py
+++ newton-raphson.py
@@ -16,3 +16,28 @@ def der(x):
 #limitando as interações para que o looop não rode eternamente
 maxInter = 20 #maximo de interações
 e = 1E-15 # erro
+i = 0 # contador de interações
+x0 = 1 # palpite inicial
+xi_1 = x0 # primeira interacao
+print(f'interacao {i} : raiz = {x0} erro = {f(x0)}')
+
+#interagindo até alcancar ou o maixmo de interações ou o erro minimo
+while abs(f(xi_1)) > e or i > maxInter:
+    i += 1
+    xi = xi_1-f(xi_1)/der(xi_1) # a Equação de Newton Raphson
+    print(f' Iteracao {i} : raiz {xi} : erro = {f(xi)}')
+    xi_1 = xi
+    
+#plotando no grafico
+x_plot = np.linspace(-2,2,1000)
+y_plot = f(x_plot)
+
+fig = plt.figure()
+plt.plot(x_plot, y_plot, c='blue')
+plt.plot(xi, f(xi),c='red',marker='o',fillstyle='none')
+plt.xlim([-2,2])
+plt.ylim([-2,2])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid()
+plt.show()
