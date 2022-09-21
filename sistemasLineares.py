28a62e15ef607dda0b7e6c85dfbf85c181d2f4d8
SUPerchokki <32721115+SUPerchokki@users.noreply.github.com>
2022-09-21 03:52:46 -0300

    primeira questão da revisão

--- sistemasLineares.py
+++ sistemasLineares.py
@@ -4,8 +4,8 @@ from numpy.linalg import inv
 from numpy.linalg import solve
 
 #solucionar sistemas lineares de duas variaveis
-A = np.array([[2,4,-5],[4,1,-5],[2,4,5]])
-B = np.array([5,2,-1])
+A = np.array([[10,3,4],[6,12,3],[6,5,17]])
+B = np.array([280,390,670])
 
 # primeiro passo é achar a inversa da A e multiplicar por B
 #inversa = inv(A)
@@ -14,5 +14,6 @@ B = np.array([5,2,-1])
 # da mesma forma é possivel resolver utilizando a função solve
 x = solve(A,B)
 print(x)
-print(f'\nX => {x[0]}\n')
-print(f'Y => {x[1]}')
+print(f'\nproduto 1 => {x[0]}\n')
+print(f'produto 2 {x[1]}')
+print(f'\nproduto 3 => {x[2]}\n')
